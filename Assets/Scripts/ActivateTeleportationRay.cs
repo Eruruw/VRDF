f13215f627254cbec9e664930f668eb1f72ed7ac
jaydented101@gmail.com <jaydented101@gmail.com>
2024-01-10 15:13:51 -0600

    Added teleportation rays. Seperated rays from hands. Created demo settings menu

---
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+public class ActivateTeleportationRay : MonoBehaviour
+{
+    public GameObject leftRay;
+    public GameObject rightRay;
+
+    public InputActionProperty leftActivate;
+    public InputActionProperty rightActivate;
+
+    public InputActionProperty leftCancel;
+    public InputActionProperty rightCancel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftRay.SetActive(leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+        rightRay.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
+    }
+}
