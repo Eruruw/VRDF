752f302684582291b3b13dc299b11051c5ca4927
jaydented101@gmail.com <jaydented101@gmail.com>
2024-01-11 02:33:31 -0600

    Added interaction rays for UI.

--- Assets/Scripts/ActivateTeleportationRay.cs
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -5,14 +5,17 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
 public class ActivateTeleportationRay : MonoBehaviour
 {
-    public GameObject leftRay;
-    public GameObject rightRay;
+    public GameObject leftTeleportationRay;
+    public GameObject rightTeleportationRay;
 
     public InputActionProperty leftActivate;
     public InputActionProperty rightActivate;
 
     public InputActionProperty leftCancel;
     public InputActionProperty rightCancel;
+
+    public XRRayInteractor rightRay;
+    public XRRayInteractor leftRay;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +25,11 @@ public class ActivateTeleportationRay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        leftRay.SetActive(leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
-        rightRay.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftVaild);
+
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightVaild);
+
+        leftTeleportationRay.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportationRay.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
