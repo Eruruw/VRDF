9868f124239b3bd9e30ee32f94f1796af9a380d8
Eruruw <spencerrochel@yahoo.com>
2024-02-01 17:35:54 -0600

    Addition of Sketchfab, plus finishing of evidence bag

--- Assets/Scripts/BagSocket.cs
+++ Assets/Scripts/BagSocket.cs
@@ -7,28 +7,41 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class BagSocket : MonoBehaviour
 {
+    public int score;
     private XRSocketInteractor socket;
     private GameObject item = null;
+    private WarrantManager manager;
+    private string warrantDesk;
+    private List<string> tagsList;
+    private bool found = false;
 
     void Start()
     {
+        GameObject warrant = GameObject.FindWithTag("manager");
         socket = GetComponent<XRSocketInteractor>();
+        manager = warrant.GetComponent<WarrantManager>();
+        warrantDesk = manager.exactDesk;
+        tagsList = manager.validEvidenceList;
     }
 
     public void SocketCheck()
     {
         IXRSelectInteractable selItem = socket.GetOldestInteractableSelected();
         item = selItem.transform.gameObject;
-        /*
-        if (item.CompareTag("evidence"))
+        EvidenceID ID = item.GetComponent<EvidenceID>();
+        if (ID.desk == warrantDesk)
         {
-            increment
+            if (tagsList.Contains(ID.type))
+            {
+                score++;
+                found = true;
+            }
         }
+        if (found)
+            found = false;
         else
-        {
-            decrement
-        }
-        */
+            score--;
         item.SetActive(false);
+        socket.enabled = false;
     }
 }
