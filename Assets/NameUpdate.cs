9bc63944459280039d43601172b4450ebf42305e
Eruruw <spencerrochel@yahoo.com>
2024-03-25 13:42:22 -0500

    Addition of evidence type and name

---
+++ Assets/NameUpdate.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NameUpdate : MonoBehaviour
+{
+    public TextMeshProUGUI namePlateText;
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Check if the collider belongs to an object with the "evidence" tag
+        if (other.CompareTag("evidence"))
+        {
+            // Access the EvidenceID script attached to the collider's GameObject
+            EvidenceID evidenceID = other.GetComponent<EvidenceID>();
+
+            // Check if the EvidenceID script is found
+            if (evidenceID != null)
+            {
+                // Set the name to the text from the TextMeshPro object
+                evidenceID.desk = namePlateText.text;
+            }
+        }
+    }
+}
